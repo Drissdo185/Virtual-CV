676e5ee2e1e8e578a3bb1a8bdc2292e37369a325
Đỗ Thành Đạt <dothanhdat185@gmail.com>
2025-01-22 22:02:07 +0700

    Build simple header

---
+++ my-app/src/page/home/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header"
+
+const MainPage = () => {
+    return(
+        <div className="min-h-screen w-full flex flex-col max-w-screen-2xl lg:px-8 md:px-8 sm:px-0 relative">
+        <Header className="w-full"/>
+
+    </div>
+    );
+    
+}
+
+export default MainPage
